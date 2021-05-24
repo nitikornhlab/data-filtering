6d5920ffd3fb1769cb783f003a0790ebb6e157af
nitikorn(biar)a <nitikorn.ong@hlabconsulting.com>
2021-05-24 11:24:19 +0700

    Add: db & env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) =>{console.log("env",configService);return ({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: configService.get('DATABASE_USER'),
+      password: configService.get('DATABASE_PASSWORD'),
+      database:'master',
+      synchronize: false,
+      extra:{
+           trustServerCertificate: true,
+         },
+    })},
+    inject: [ConfigService],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
