2d8bfbbc0436f45ebee829c5bb3f988f738cdd0d
nitikorn(biar)a <nitikorn.ong@hlabconsulting.com>
2021-05-28 14:02:56 +0700

    Add: query with transaction case

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,20 +8,39 @@ export class AppService {
     // get a connection and create a new query runner
     const connection = getConnection();
     const queryRunner = connection.createQueryRunner();
-    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-    try {
-      const result = await queryRunner.manager.query(
-        `EXEC sys.sp_set_session_context @key = N'language', @value = ${Math.random()};`,
-      );
-      await delay(5000);
-      return result;
-    } finally {
-      await queryRunner.manager.query(
-        `EXEC sys.sp_set_session_context 'language', null`,
-      );
-      // you need to release query runner which is manually created:
-      await queryRunner.release();
-    }
+
+    return await getConnection().transaction(
+      async (transactionalEntityManager) => {
+        const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+        try {
+          const result = await queryRunner.manager.query(
+            `EXEC sys.sp_set_session_context @key = N'language', @value = ${Math.random()};`,
+          );
+          await delay(5000);
+          return result;
+        } finally {
+          await queryRunner.manager.query(
+            `EXEC sys.sp_set_session_context 'language', null`,
+          );
+          // you need to release query runner which is manually created:
+          await queryRunner.release();
+        }
+      },
+    );
+    // try {
+    //   const result = await queryRunner.manager.query(
+    //     `EXEC sys.sp_set_session_context @key = N'language', @value = ${Math.random()};`,
+    //   );
+    //   await delay(5000);
+    //   return result;
+    // } finally {
+    //   await queryRunner.manager.query(
+    //     `EXEC sys.sp_set_session_context 'language', null`,
+    //   );
+    //   // you need to release query runner which is manually created:
+    //   await queryRunner.release();
+    // }
   }
 
   async get2(): Promise<string> {
@@ -29,15 +48,19 @@ export class AppService {
     const connection = getConnection();
     const queryRunner = connection.createQueryRunner();
 
-    try {
-      const result = await queryRunner.manager.query(
-        ` SELECT SESSION_CONTEXT(N'language') as language;   `,
-      );
-      return result;
-    } finally {
-      // you need to release query runner which is manually created:
-      await queryRunner.release();
-    }
+    return await getConnection().transaction(
+      async (transactionalEntityManager) => {
+        try {
+          const result = await queryRunner.manager.query(
+            ` SELECT SESSION_CONTEXT(N'language') as language;   `,
+          );
+          return result;
+        } finally {
+          // you need to release query runner which is manually created:
+          await queryRunner.release();
+        }
+      },
+    );
   }
 
   async getUser(userId?: number) {
